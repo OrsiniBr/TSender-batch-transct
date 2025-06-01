bd1fa5cb66a01b6b146f9a9f5f68415d07cc4ada
udeh udochukwu <udochukwugodwin13@gmail.com>
2025-06-01 01:36:34 +0100

    config for fleek deploy

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ npm init @rainbow-me/rainbowkit@latest
 pnpm add @rainbow-me/rainbowkit wagmi viem@2.x @tanstack/react-query
 SXA5ADIVKXYK4CZKR5YKJ5L3EI
 pnpm exec playwright test --ui
+
+
+create a absic react app
+connect our wallet with a nicer connect application
+implement this functon
+e2e testing
+deploy to fleek
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: 'out',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+
+  }, 
+  basePath: "",
+  assetPrefix: './',
+  trailingSlash: true, // Add trailing slashes to URLs
 };
 
 export default nextConfig;
